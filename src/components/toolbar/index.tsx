020d372adb7b9dcad1aa8e3aec1287da3a9e827b
jiangyf@rimepevc.com <jiangyf@rimepevc.com>
2021-07-14 19:31:26 +0800

    .

--- src/components/toolbar/index.tsx
+++ src/components/toolbar/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 interface ToolbarProps {
   zoom?: boolean;
+  scale?: boolean;
+  pagination?: boolean;
 }
 
 const Toolbar: React.FunctionComponent<ToolbarProps> = (props) => {
