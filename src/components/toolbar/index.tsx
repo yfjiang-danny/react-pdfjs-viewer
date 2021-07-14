40e872e40fac624a2e2aebfba0887361ea5a9fa7
jiangyf@rimepevc.com <jiangyf@rimepevc.com>
2021-07-14 15:11:51 +0800

    .

---
+++ src/components/toolbar/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface ToolbarProps {}
+
+const Toolbar: React.FunctionComponent<ToolbarProps> = (props) => {
+  return <>Toolbar Component</>;
+};
+
+export default Toolbar;
---
+++ src/containers/container.ts
@@ -0,0 +1,9 @@
+import { createContainer } from "unstated-next";
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function usePDFViewer() {
+  return {};
+}
+
+const PDFViewerContainer = createContainer(usePDFViewer);
+export default PDFViewerContainer;
